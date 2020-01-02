8a9c7bdebe8d9cc2caf019bf4dcea2da166abdf8
harshada-1996 <harshada.chinchole@anchanto.com>
2020-01-02 18:08:28 +0530

    ajax and json

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,10 @@
 class PagesController < ApplicationController
     def show
+       pages=params[:form_id]
+       byebug
+       #render json: @pages.to_json(only: [:email, :psw])
+       render json: {response => true, pages: pages}.to_json
+
     end
     def signup
     end
--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,10 @@ Rails.application.routes.draw do
   resources :students
   resources :tasks
   root 'pages#signup'
+
+  get 'pages/show' ,to: 'pages#show'
+  post 'pages/show' ,to: 'pages#show'
+  
   resources :users
   
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
